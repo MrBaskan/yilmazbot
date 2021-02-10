5d35f30410e32fdb3470b6b2c7f78d9974cd54a6
MrBaskan <71145384+MrBaskan@users.noreply.github.com>
2021-02-10 19:17:25 +0300

    Update botlist-başvuruayarla.js

--- komutlar/botlist-başvuruayarla.js
+++ komutlar/botlist-başvuruayarla.js
@@ -3,12 +3,6 @@ const client = new Discord.Client();
 const db = require('quick.db');
 module.exports.run = async (client, message, args) => {
 
-let kontrol = await db.fetch(`dil_${message.guild.id}`);
-  if (kontrol == "tr") {
-  let p = args[0];
-  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || `-`
-  let onlycode = args.slice(0).join(' ');
-  
   if(!message.member.hasPermission(`ADMINISTRATOR`)) return message.channel.send(`Bu Komutu Kullanabilmek için **Yönetici** Yetkisine Sahip Olmalısın.`)
     if(db.has(`bot-ekle_${message.guild.id}`)) return message.channel.send("Başvuru Kanalı Ayarlı, Sıfırlamak İçin : y!başvuru-kanal-sıfırla")
     let botekle = message.mentions.channels.first();
@@ -16,29 +10,16 @@ let kontrol = await db.fetch(`dil_${message.guild.id}`);
     db.set(`bot-ekle_${message.guild.id}`, botekle.id)
     message.channel.send("Başvuru Kanalı Ayarlandı.")
 
-    }
- else {
-  let p = args[0];
-  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || `-`
-  let onlycode = args.slice(0).join(' ');
-  
-  if(!message.member.hasPermission(`ADMINISTRATOR`)) return message.channel.send(`You must have **Administrator** privilege to use this command.`)
-    if(db.has(`bot-ekle_${message.guild.id}`)) return message.channel.send("Application channel set, to reset: -application-channel-reset")
-    let botekle = message.mentions.channels.first();
-    if(!botekle) return message.channel.send("Specify a channel.")
-    db.set(`bot-ekle_${message.guild.id}`, botekle.id)
-    message.channel.send("Application channel is set.")
-
-  }} 
+  }
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["application-channel-set"],
+  aliases: [],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'başvuru-kanalı-ayarla', 
+  name: 'başvuru-kanalı-ayarla',
   description: "",
   usage: ''
 };
